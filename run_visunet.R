c8f1b97ee2e1d6de071c8ab1a8750b6aa89bea37
GirishPulinkala <98015303+GirishPulinkala@users.noreply.github.com>
2025-11-12 16:51:52 +0100

    How to write a file into csv

--- run_visunet.R
+++ run_visunet.R
@@ -7,3 +7,8 @@ load_all("~/Documents/Forskningspraktik/VisuNet")
 ros <- rosetta(autcon)
 # VisuNet with GO annotations
 vis <- visunet(ros$main, addGO = TRUE, GO_ontology = "MF", NodeSize = "sum")
+
+
+#to write a file
+library(data.table)
+fwrite(vis$all$nodes,'~/Documents/gFunctions/file.csv')
