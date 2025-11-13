960101ff59d87d7b72b92820de923d4b18a8bd18
tildsand <146180605+tildsand@users.noreply.github.com>
2025-11-13 16:00:23 +0100

    Add edge color and width

--- Scripts (Should be integrated later)/VisuNetXCytoscape.R
+++ Scripts (Should be integrated later)/VisuNetXCytoscape.R
@@ -22,14 +22,21 @@ for (net_name in names(Autism_Visunet)) {
 
   style.name = paste(net_name, '_style')
   defaults <- list(NODE_SHAPE="circle")
+  
+  # Node styles
   nodeLabels <- mapVisualProperty('node label','label','p')
   nodeSize <- mapVisualProperty('node size','meanSupp','c',c(min(network$nodes$meanSupp),max(network$nodes$meanSupp)), c("30","75"))
   nodeFills <- mapVisualProperty('node fill color', 'color.background', 'p')
   nodeBorderWidth <- mapVisualProperty('node border width', 'borderWidth', 'p')
   nodeBorderColor <- mapVisualProperty('node border paint', 'color.border', 'p')
+  
+  # Edge styles
+  edgeColor <- mapVisualProperty('edge stroke unselected paint','color','p')
   edgeLabels <- mapVisualProperty('edge label','title','p')
   edgeVisibility <- mapVisualProperty('edge label transparency','selected','d', c('true', 'false'), c(255,0))
+  edgeWidth <- mapVisualProperty('edge width', 'width', 'p')
+  
   try(deleteVisualStyle(style.name), silent=TRUE)
-  createVisualStyle(style.name, defaults, list(nodeSize, nodeFills, nodeLabels, nodeBorderWidth, nodeBorderColor, edgeLabels, edgeVisibility))
+  createVisualStyle(style.name, defaults, list(nodeSize, nodeFills, nodeLabels, nodeBorderWidth, nodeBorderColor, edgeColor, edgeLabels, edgeVisibility, edgeWidth))
   setVisualStyle(style.name)
 }
