c856bdd5ed1852d77575094157c1665fe67149cf
Ibraalnuss <ibra818@gmail.com>
2025-11-23 14:36:10 +0100

    Auto-create and reuse VisuNet object

--- Scripts (Should be integrated later)/run_visunet.R
+++ Scripts (Should be integrated later)/run_visunet.R
@@ -1,14 +1,47 @@
 library(devtools)
 library(RCy3)
+library(R.ROSETTA)
 
+# Ladda VisuNet-paketet
 load_all("/Users/ibraheem/Desktop/visu_to_cyto/VisuNet.v.1.1")
+
+# Ladda funktionen visunetcyto
 source("/Users/ibraheem/Desktop/visu_to_cyto/VisuNet.v.1.1/visu_to_cyto.R")
 
-cytoscapePing()  # kolla att Cytoscape är igång
+# Kolla att Cytoscape är igång
+cytoscapePing()
 
-# Ladda VisuNet objektet från tidigare körning
+# Skapa eller läs in VisuNet-objektet
 if (!exists("vis")) {
-  vis <- readRDS("visunet_headless.rds")
+  if (file.exists("visunet_headless.rds")) {
+    # Läs in sparat VisuNet-objekt
+    vis <- readRDS("visunet_headless.rds")
+  } else {
+    # Använd Cytoscapes egna GO-funktioner → ingen GO i VisuNet
+    addGO <- FALSE
+    
+    # Kör ROSETTA på datan (autcon måste finnas i environment)
+    ros <- rosetta(autcon)
+    
+    # Kör visunet i headlessläge (ingen popup)
+    options(
+      visunet.headless     = TRUE,
+      shiny.launch.browser = FALSE,
+      viewer               = NULL,
+      browser              = function(...) {}
+    )
+    
+    # Skapa VisuNet objekt
+    vis <- visunet(
+      ros$main,
+      addGO       = addGO,
+      GO_ontology = "MF",
+      NodeSize    = "sum"
+    )
+    
+    # Spara VisuNet-objektet för framtida körningar
+    saveRDS(vis, "visunet_headless.rds")
+  }
 }
 
 # Skicka VisuNet objektet till Cytoscape
@@ -17,4 +50,4 @@ visunetcyto(
   title = "Autism_VisuNet_optA"
 )
 
-# Klart, nätverken ska nu finnas i Cytoscape
+# Klart: Nätverken ska nu finnas i Cytoscape
